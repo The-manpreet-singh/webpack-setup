b4ccdcbfd374416280dd38a130a3500eed241e6c
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-11 11:59:56 +0530

    adding rules

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,26 @@ const path = require('path');
 
 module.exports = {
     devtools: 'cheap-module-eval-source-map',
+   
     entry: './src/index.js',
+   
     output: {
         path:'',
         filename:'bundle.js',
         publicPath: path.resolve(__dirname, 'dist') 
+    },
+
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: ,
+                exclude: /node_modules/
+            }
+        ]
     }
 }
