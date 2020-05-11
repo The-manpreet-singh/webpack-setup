9ab7a3ad70b059bd1239cf60aa7f10124dac5f3f
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-11 11:09:10 +0530

    add hoc

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, {Component} from 'react';
 import {Link, Route} from 'react-router-dom';
 
 import Users from './containers/Users';
-import Pizza from './containers/Pizza';
+import asyncComponent from './hoc/asyncComponent';
 
+const AsyncPizza = asyncComponent(()=>{
+    return import('./containers/Pizza.js')
+});
 class App extends Component {
     render() {
         return (
@@ -14,7 +17,7 @@ class App extends Component {
                 </div>
                 <div>
                   <Route path='/' exact component={Users} />
-                  <Route path='/pizza' component={Pizza} />
+                  <Route path='/pizza' component={AsyncPizza} />
                 </div>
             </div>
         )
