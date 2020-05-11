4761b0370d49ab8e138f2ff3b76bd68c5fdf3266
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-11 12:31:35 +0530

    adding css loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
     devtools: 'cheap-module-eval-source-map',
@@ -21,6 +22,37 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader' ,
                 exclude: /node_modules/
+            },
+
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'style-loader' },
+
+                    {  
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders:1,
+                            module: true,
+                            localIdentName: '[name]__[local]__[hash:base64:5]'
+                        }
+                     },
+ 
+                     {
+                         loader: 'postcss-loader',
+                         options: {
+                             ident: 'postcss',
+                             plugins : () => [
+                                 autoprefixer({
+                                    "> 1%",
+                                    "last 2 versions"
+                                 })
+                             ]
+                         }
+                     }
+
+                ]
             }
         ]
     }
